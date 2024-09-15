c701a02a0bc6474fb46073ad7b7eba40a84881f3
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-09-15 13:33:33 +0530

    Modified socket context's peer connections function for deploying

--- src/Context/SocketContext.tsx
+++ src/Context/SocketContext.tsx
@@ -95,7 +95,6 @@ export const SocketProvider: React.FC<IProps> = ({ children }) => {
     const userId = UUIDv4();
     const newPeer = new Peer(userId, {
       host: serverConfig.VITE_PEER_SERVER_HOST,
-      port: serverConfig.VITE_PEER_SERVER_PORT,
       path: serverConfig.VITE_PEER_SERVER_PATH,
     });
 
